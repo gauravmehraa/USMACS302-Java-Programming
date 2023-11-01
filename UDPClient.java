f9b3c09cc9242b707ed9cb621a859d118c87099b
gauravmehraa <gauravmehra158@gmail.com>
2023-11-02 01:08:01 +0530

    Added 2-way communication

--- UDPClient.java
+++ UDPClient.java
@@ -4,6 +4,7 @@ public class UDPClient{
 	static int serverport = 8888;
 	public static void main(String args[]){
 		try{
+			//Send to server
 			DatagramSocket s = new DatagramSocket();
 			InetAddress serveraddress = InetAddress.getByName("localhost");
 			String message = "Sample message from client";
@@ -11,6 +12,13 @@ public class UDPClient{
 
 			DatagramPacket p = new DatagramPacket(send, send.length, serveraddress, serverport);
 			s.send(p);
+			
+			//Receive from server
+			byte[] buffer = new byte[1024];
+			p = new DatagramPacket(buffer, buffer.length);
+			s.receive(p);
+			message = new String(p.getData(), 0, p.getLength());
+			System.out.println("Received from server: "+message);
 		}
 		catch(Exception e){
 			e.printStackTrace();
--- UDPServer.java
+++ UDPServer.java
@@ -6,11 +6,18 @@ public class UDPServer{
 			DatagramSocket s = new DatagramSocket(8888);
 			byte[] buffer = new byte[1024];
 			while(true){
+
+				//Receive from client
 				DatagramPacket p = new DatagramPacket(buffer, buffer.length);
 				s.receive(p);
 
 				String message = new String(p.getData(), 0, p.getLength());
 				System.out.println("Received message: " +message);
+
+				//Send to client
+				byte[] send = "Server to client".getBytes();
+				p = new DatagramPacket(send, send.length, p.getAddress(), p.getPort());
+				s.send(p);
 			}
 		}
 		catch(Exception e){
